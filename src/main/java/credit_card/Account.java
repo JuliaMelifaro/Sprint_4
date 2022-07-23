14779ca13ffde3b16b04f926eafb9ddbf09d66da
Julia Tsyganok <Monohmatik@ya.ru>
2022-07-23 23:14:54 +0300

    Account + Praktikum classes

---
+++ src/main/java/credit_card/Account.java
@@ -0,0 +1,37 @@
+package credit_card;
+
+public class Account {
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    // Проверяет можно ли печатать имя на банковской карте
+    public boolean checkNameToEmboss() {
+        boolean isChecked = checkNameLength() && checkAmountOfSpaces() && checkSpacesPlace();
+        return isChecked;
+    }
+
+    // Проверяет что количество символом [3,19]
+    private boolean checkNameLength() {
+        if (name.length() >= 3) {
+            if (name.length() <= 19) {return true;}}
+        return false;
+    }
+
+    // Проверяет что в строке только один пробел
+    private boolean checkAmountOfSpaces() {
+        String withoutSpaces = name.replace(" ", "");
+        if (withoutSpaces.length() == (name.length() - 1)) return true;
+        return false;
+    }
+
+    // Проверяет что пробел расположен не в начале или конце строки
+    private boolean checkSpacesPlace() {
+        if (name.equals(name.trim())) return true;
+        return false;
+    }
+
+
+}
